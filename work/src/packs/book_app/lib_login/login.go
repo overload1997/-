a2783853cf171534dbb06fe92ebb0f6e2f9a98f6
overload <duhangken@163.com>
2018-08-03 16:18:48 +0800

    modify lib_login.go

--- work/src/packs/book_app/lib_login/login.go
+++ work/src/packs/book_app/lib_login/login.go
@@ -13,26 +13,27 @@ import (
 
 const (
 	username = "root"
-	password = "qw578470347"
+	password = "123456789+0"
 	port     = "mysql"
 	dbname   = "lib_sql"
 	conpro   = "tcp"
 )
 
 type Respond struct {
-	Message string
+	code    int
+	message string
 }
 
 //Login接口
 func Login(w http.ResponseWriter, r *http.Request) {
 	//获取表单
-	user := r.FormValue("user")
+	phone := r.FormValue("phone")
 	//password := overload_aes.Unpackkey(r.FormValue("password"))
 	password := r.FormValue("password")
-	fmt.Printf("user:%s password:%s\n", user, password)
+	fmt.Printf("id:%s password:%s\n", phone, password)
 	//链接数据库
 	//sqlpath := strings.Join([]string{username, ":", password, "@", conpro, "(localhost", ":", port, ")/", dbname, "?charset=utf8"}, "")
-	db, err := sql.Open("mysql", "root:qw578470347@tcp(localhost:mysql)/lib_sql")
+	db, err := sql.Open("mysql", "root:qw578470347@tcp(localhost:mysql)/book_app")
 	defer db.Close()
 	println("尝试ping the 数据库")
 	if err := db.Ping(); err != nil {
@@ -41,31 +42,32 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Println("连接成功")
 	//查询处理
-	rows, err := db.Query("select stu_password from student where stu_id=" + user)
+	rows, err := db.Query("select stu_password from user_info where phone=\"" + phone + "\"")
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
 	userexit := false //数据库中存不存在该账户
 	respond := &Respond{}
-	for rows.Next() {
-		userexit = true
-		var psw string
-		err := rows.Scan(&psw)
-		if err != nil {
-			log.Fatal(err)
-			return
-		}
-		if psw != password {
-			respond.Message = "请输入正确的密码!"
-			//return
-		} else {
-			respond.Message = "密码正确!"
-			//return
-		}
+	userexit = true
+	var psw string
+	err = rows.Scan(&psw)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	if psw != password {
+		respond.code = 11
+		respond.message = "请输入正确的密码!"
+		//return
+	} else {
+		respond.code = 0
+		respond.message = "密码正确!"
+		//return
 	}
 	if !userexit {
-		respond.Message = "请输入正确的帐号"
+		respond.code = 10
+		respond.message = "请输入正确的帐号"
 	}
 	re, err1 := json.Marshal(respond)
 	if err1 != nil {
