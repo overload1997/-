bcd9539ddf5abe08e572e818d07ea5f96c6dd051
overload <duhangken@163.com>
2018-08-04 21:39:00 +0800

    modify api-indetify_code

--- work/src/packs/book_app/lib_main/main.go
+++ work/src/packs/book_app/lib_main/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"packs/book_app/lib_login"
 	"packs/book_app/lib_register"
+	"packs/book_app/send_message"
 )
 
 const ()
@@ -12,6 +13,7 @@ const ()
 func main() {
 	http.HandleFunc("/user/login", lib_login.Login)
 	http.HandleFunc("/user/register", lib_register.Register)
+	http.HandleFunc("/user/identify_code", send_message.Send_message)
 	err := http.ListenAndServe(":1521", nil)
 	if err != nil {
 		log.Fatal(err)
